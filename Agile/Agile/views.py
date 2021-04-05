abf1c2e581e5e9a0c4ce397362ecf771b8389293
yarinab1 <57402773+yarinab1@users.noreply.github.com>
2021-04-05 13:23:45 +0300

    update

--- Agile/Agile/views.py
+++ Agile/Agile/views.py
@@ -59,14 +59,3 @@ def ProgrammerHomePage(request):
     return render(request,"Agile/ProgrammerHomePage.html")
 def ClientHomePage(request):
     return render(request,"Agile/CUSTOMER.html")
-def NewProjectPage(request):
-    return render(request,'Agile/NewProjectPage.html')
-def CreateProjDone(response):
-    if response.method == 'POST':
-        SV = db.projects
-        projects = {
-            "ProjectName" : response.POST.get('ProjectName'),
-        }
-        SV.insert_one(projects)
-        client.close()
-    return render(response,'Agile/CreateProjDone.html')
