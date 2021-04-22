b9d63775f1ee40d211a1e45187dc4517d172a1c6
Guy Cohen <guyco8@ac.sce.ac.il>
2021-04-22 12:07:07 +0300

    Update test file

--- Agile/tests.py
+++ Agile/tests.py
@@ -5,6 +5,18 @@ from Agile.views import *
 class Test(SimpleTestCase):
 
     def test_homepage_url(self):
-        response = self.client.get('/')
+        response = self.client.get('')
+        self.assertEquals(response.status_code, 200)
+
+    def test_SIGNUP_url(self):
+        response = self.client.get('/SIGNUP')
+        self.assertEquals(response.status_code, 200)
+
+    def test_LOGIN_url(self):
+        response = self.client.get('/LOGIN')
+        self.assertEquals(response.status_code, 200)
+
+    def test_SignUpDone_url(self):
+        response = self.client.get('/SignUpDone')
         self.assertEquals(response.status_code, 200)
 
