218ab2caa4cc761587746dd0af9db5c57863f10b
Guy Cohen <guyco8@ac.sce.ac.il>
2021-05-30 17:28:40 +0300

    send mail - bug fix

--- send_mail.py
+++ send_mail.py
@@ -2,8 +2,8 @@
 from Agile.settings import EMAIL_HOST_USER
 from django.core.mail import send_mail
 def sendmail():
-    try:
-        send_mail('Jenkins Build Failed', 'The last build failed, check it!', EMAIL_HOST_USER, ['yarinaf1@gmail.com','gaico070@gmail.com'], fail_silently=True)
+    send_mail('Jenkins Build Failed', 'The last build failed, check it!', EMAIL_HOST_USER, ['yarinaf1@gmail.com','gaico070@gmail.com'], fail_silently=True)
+
 if __name__ == '__main__':
     try:
         sendmail()
