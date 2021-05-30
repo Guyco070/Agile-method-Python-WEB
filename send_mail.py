ef74bfca31dd785509c7011ce9b3d1c5b15fad00
yarinab1 <yarinab1@ac.sce.ac.il>
2021-05-30 17:25:55 +0300

    fix bug-send mail

--- send_mail.py
+++ send_mail.py
@@ -1,9 +1,11 @@
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import smtplib
-import os
-from sys import argv
+
 from Agile.settings import EMAIL_HOST_USER
-def send_mail():
+from django.core.mail import send_mail
+def sendmail():
     try:
         send_mail('Jenkins Build Failed', 'The last build failed, check it!', EMAIL_HOST_USER, ['yarinaf1@gmail.com','gaico070@gmail.com'], fail_silently=True)
+if __name__ == '__main__':
+    try:
+        sendmail()
+    except Exception as e:
+        print(e)
